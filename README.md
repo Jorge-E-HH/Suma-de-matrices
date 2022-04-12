dd1de8cef1bf3d584ed56d618c567cfab323c42e
Jorge E. HH <Jaydersmk@gmail.com>
2022-04-12 15:19:40 -0500

    Add dev c

---
+++ README.md
@@ -0,0 +1,5 @@
+# Suma-de-matrices
+Programa en  C++ para sumar dos matrices
+
+ # Créditos
+  [J. Hernandez](https://github.com/Jorge-E-HH) autor original del contenido.
---
+++ main.cpp
@@ -0,0 +1,66 @@
+/*
+
+	Este programa suma dos matrices, ejem: 
+	
+	| 2 1 | + | 1 2 | = | 3 3 | 
+	| 1 2 |   | 2 1 |   | 3 3 |
+*/
+#include <iostream>
+using namespace std; 
+
+int main(int argc, char ** argv)
+{
+	//Declaración de variables //
+	int fila = 0, columna = 0;
+	float matriz_a[fila][columna], matriz_b[fila][columna], suma[fila][columna];
+	//Pedir al usuario el numero de filas y columnas de las  matrices recordar que  el número de filas de A debe ser igual al de B //
+	cout<<"Numero de columnas"<<endl;
+	cin>>columna;
+	cout<<"Numero de filas"<<endl;
+	cin>>fila;
+	//Ingresar los  datos de la  matriz  A//
+	cout<<"Ingrese los valores de la matriz A:"<<endl;
+	
+	for(int i = 0; i < fila; i++)
+	{
+		for(int j = 0; j < columna; j++)
+		{
+			cout<<"fila: "<<i<<" columna: "<<j<<" ";
+			cin>>matriz_a[i][j];
+		}
+	}
+	
+	//Ingresar los  datos de la  matriz  B//
+	cout<<"Ingrese los valores de la matriz B:"<<endl;
+	
+	for(int i = 0; i < fila; i++)
+	{
+		for(int j = 0; j < columna; j++)
+		{
+			cout<<"fila: "<<i<<" columna: "<<j<<" ";
+			cin>>matriz_b[i][j];
+		}
+	}
+	
+	//Sumar matriz A + matriz B
+	
+	for(int i = 0; i < fila; i++)
+	{
+		for(int j = 0; j < columna; j++)
+		{
+			suma[i][j] = matriz_a[i][j] + matriz_b[i][j];
+		}
+	}
+	//Mostrar el restado de la suma en la pantalla//
+	cout<<"La suma de las matrices es: "<< endl;
+	
+	for(int i = 0; i < fila; i++)
+	{	cout<<"| ";
+		for(int j = 0; j < columna; j++)
+		{
+			cout<<suma[i][j]<<" ";
+		}
+		cout<<"|"<<endl;
+	}
+	
+}
